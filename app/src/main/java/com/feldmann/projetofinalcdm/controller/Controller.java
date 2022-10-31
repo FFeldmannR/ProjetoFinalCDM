a72aa655336f95e6b22d0a99e78e0984ea13d5ae
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-10-31 11:27:30 -0300

    na classe controller/interface view, criado metodos getActivity e getContext

--- app/src/main/java/com/feldmann/projetofinalcdm/controller/Controller.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/controller/Controller.java
@@ -1,7 +1,11 @@
 package com.feldmann.projetofinalcdm.controller;
 
+import android.app.Activity;
+import android.content.Context;
+
 public class Controller {
     public interface view{
-        //
+        public Activity getActivity();
+        public Context getContext();
     }
 }
