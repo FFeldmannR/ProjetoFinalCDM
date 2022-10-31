0caeaccb79680bb238feb2cfee07d950ae868890
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-10-31 11:34:44 -0300

    feito a chamada de Controller.view na MainActivity e a chamada de Controller.msg

--- app/src/main/java/com/feldmann/projetofinalcdm/MainActivity.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/MainActivity.java
@@ -1,13 +1,28 @@
 package com.feldmann.projetofinalcdm;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.feldmann.projetofinalcdm.controller.Controller;
+import com.feldmann.projetofinalcdm.controller.MsgController;
+
+public class MainActivity extends AppCompatActivity implements Controller.view {
+    private final String tagLog = this.getClass().getName().toString();
+    private Controller.msg msg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.msg = new MsgController(getContext(), tagLog);
+        msg.logD("onCreate");
     }
+    @Override
+    public Activity getActivity() { return this; }
+    @Override
+    public Context getContext() { return this; }
 }
