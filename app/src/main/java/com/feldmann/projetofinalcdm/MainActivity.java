ff23845c8b7c678a6fbd40db545e94eabe9e89ad
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-10-31 11:42:09 -0300

    ajustado visualização do codigo na main

--- app/src/main/java/com/feldmann/projetofinalcdm/MainActivity.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/MainActivity.java
@@ -1,19 +1,16 @@
 package com.feldmann.projetofinalcdm;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-
 import com.feldmann.projetofinalcdm.controller.Controller;
 import com.feldmann.projetofinalcdm.controller.MsgController;
 
 public class MainActivity extends AppCompatActivity implements Controller.view {
     private final String tagLog = this.getClass().getName().toString();
     private Controller.msg msg;
-
-
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements Controller.view {
         this.msg = new MsgController(getContext(), tagLog);
         msg.logD("onCreate");
     }
+    //
     @Override
     public Activity getActivity() { return this; }
     @Override
