72055366aea3daafa7144bbf6cf772316a702208
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-11-01 08:13:30 -0300

    alterado chamada de ViewController em ListaDeCompras

--- app/src/main/java/com/feldmann/projetofinalcdm/views/ListaDeCompras.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/ListaDeCompras.java
@@ -1,23 +1,23 @@
 package com.feldmann.projetofinalcdm.views;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import com.feldmann.projetofinalcdm.R;
 import com.feldmann.projetofinalcdm.controller.Controller;
 import com.feldmann.projetofinalcdm.controller.MsgController;
+import com.feldmann.projetofinalcdm.controller.ViewController;
 
-public class ListaDeCompras extends AppCompatActivity implements Controller.view {
+public class ListaDeCompras extends AppCompatActivity{
     private final String tagLog = this.getClass().getName().toString();
     private Controller.msg msg;
+    private Controller.view view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_de_compras);
-        this.msg = new MsgController(getContext(), tagLog);
+        this.view = new ViewController(this, this);
+        this.msg = new MsgController(view.getContext(), tagLog);
         msg.logD("onCreate");
     }
 
@@ -26,9 +26,4 @@ public class ListaDeCompras extends AppCompatActivity implements Controller.view
         super.onResume();
         msg.logD("onResume");
     }
-
-    @Override
-    public Activity getActivity() { return this; }
-    @Override
-    public Context getContext() { return this; }
 }
