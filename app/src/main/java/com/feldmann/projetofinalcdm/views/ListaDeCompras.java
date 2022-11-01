556315552c5418661c31041f674c6afbf37090a3
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-11-01 07:58:34 -0300

    feita a chamada de msg e view em ListaDeCompras

--- app/src/main/java/com/feldmann/projetofinalcdm/views/ListaDeCompras.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/ListaDeCompras.java
@@ -2,15 +2,27 @@ package com.feldmann.projetofinalcdm.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-
 import com.feldmann.projetofinalcdm.R;
+import com.feldmann.projetofinalcdm.controller.Controller;
+import com.feldmann.projetofinalcdm.controller.MsgController;
 
-public class ListaDeCompras extends AppCompatActivity {
+public class ListaDeCompras extends AppCompatActivity implements Controller.view {
+    private final String tagLog = this.getClass().getName().toString();
+    private Controller.msg msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_de_compras);
+        this.msg = new MsgController(getContext(), tagLog);
+        msg.logD("onCreate");
     }
+
+    @Override
+    public Activity getActivity() { return this; }
+    @Override
+    public Context getContext() { return this; }
 }
