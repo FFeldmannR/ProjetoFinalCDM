1d81c4270286ba555cdffaad0d4f9d7f307aa1b6
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-11-03 13:15:27 -0300

    feature/ criado metodos

--- app/src/main/java/com/feldmann/projetofinalcdm/repository/ListasRepository.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/repository/ListasRepository.java
@@ -1,4 +1,25 @@
 package com.feldmann.projetofinalcdm.repository;
 
+import com.feldmann.projetofinalcdm.model.Lista;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListasRepository {
+    private List<Lista> listas;
+    private static ListasRepository instance = null;
+
+    public ListasRepository() {
+        if (instance == null){
+            listas = new ArrayList<>();
+        }
+    }
+
+    public static ListasRepository getInstance(){
+        instance = new ListasRepository();
+        return instance;
+    }
+
+    public List<Lista> getListas() {
+        return listas;
+    }
 }
