784745922c3d2a401a2f54c8531edb3dc1601a0e
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-11-03 12:40:07 -0300

    criado classe de model Lista

---
+++ app/src/main/java/com/feldmann/projetofinalcdm/model/Lista.java
@@ -0,0 +1,17 @@
+package com.feldmann.projetofinalcdm.model;
+
+public class Lista {
+    private int id;
+    private String nome;
+
+    public Lista(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    //
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+}
