4e679ae1df33dcde68a9f8b3671a6c0cc92d86e8
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-04 16:47:10 -0300

    feature/ feito a chamada para criar user

--- app/src/main/java/com/feldmann/projetofinalcdm/views/CadastroActivity.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/CadastroActivity.java
@@ -2,21 +2,29 @@ package com.feldmann.projetofinalcdm.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.feldmann.projetofinalcdm.R;
+import com.feldmann.projetofinalcdm.controller.CadastroController;
 import com.feldmann.projetofinalcdm.controller.Controller;
 import com.feldmann.projetofinalcdm.controller.MsgController;
 import com.feldmann.projetofinalcdm.controller.ViewController;
+import com.feldmann.projetofinalcdm.repository.DBListas;
 
 public class CadastroActivity extends AppCompatActivity {
     private final String tagLog = this.getClass().getName().toString();
     private Controller.msg msg;
     private Controller.view view;
+    private DBListas db;
+    private Controller.controllerCadastro cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
         this.instanceController();
+        db = new DBListas(view.getContext());
         msg.logD("onCreate");
     }
 
@@ -24,10 +32,13 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         msg.logD("onResume");
+        cadastro.getCampos((EditText) findViewById(R.id.etLoginC), (EditText) findViewById(R.id.etSenhaC));
+        cadastro.addToDB((Button) findViewById(R.id.btnCadastrarC), db.getWritableDatabase());
     }
 
     private void instanceController() {
         this.view = new ViewController(this, this);
         this.msg = new MsgController(view.getContext(), tagLog);
+        this.cadastro = new CadastroController(msg);
     }
 }
