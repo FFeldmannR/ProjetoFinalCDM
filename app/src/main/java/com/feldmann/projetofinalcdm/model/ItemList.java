dbb6231ddbec0305ea98d31d8c13e92ad8a52d88
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-11-04 10:47:45 -0300

    feature/ criado variaveis e metodos

--- app/src/main/java/com/feldmann/projetofinalcdm/model/ItemList.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/model/ItemList.java
@@ -1,4 +1,22 @@
 package com.feldmann.projetofinalcdm.model;
 
 public class ItemList {
+    private int id;
+    private String nomeLista;
+    private boolean check[];
+
+    public ItemList(int id, String nomeLista, boolean[] check) {
+        this.id = id;
+        this.nomeLista = nomeLista;
+        this.check = check;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    //
+    public String getNomeLista() { return nomeLista; }
+    public void setNomeLista(String nomeLista) { this.nomeLista = nomeLista; }
+    //
+    public boolean[] getCheck() { return check; }
+    public void setCheck(boolean[] check) { this.check = check; }
 }
