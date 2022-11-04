e12f8064b3778590779cea4df9b717a9d5380391
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-04 15:35:54 -0300

    feature/ criado controller para login

--- app/src/main/java/com/feldmann/projetofinalcdm/controller/Controller.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/controller/Controller.java
@@ -2,8 +2,8 @@ package com.feldmann.projetofinalcdm.controller;
 
 import android.app.Activity;
 import android.content.Context;
+import android.widget.Button;
 import android.widget.ImageButton;
-
 import androidx.recyclerview.widget.RecyclerView;
 
 public class Controller {
@@ -14,6 +14,9 @@ public class Controller {
     public interface msg{
         public void messageToast(String msg);
         public void logD(String msg);
+    }
+    public interface controllerLogin{
+        public void paraTelaLista(Button btn);
     }
     public interface controllerListas {
         public void addList(ImageButton imgBtn);
