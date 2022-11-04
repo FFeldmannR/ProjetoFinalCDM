b94a6740ce3f46d618fa84cfdd74d7547c939f03
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-04 16:00:16 -0300

    feature/ feito a chamada de LoginController

--- app/src/main/java/com/feldmann/projetofinalcdm/views/LoginActivity.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/LoginActivity.java
@@ -2,8 +2,11 @@ package com.feldmann.projetofinalcdm.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
 import com.feldmann.projetofinalcdm.R;
 import com.feldmann.projetofinalcdm.controller.Controller;
+import com.feldmann.projetofinalcdm.controller.LoginController;
 import com.feldmann.projetofinalcdm.controller.MsgController;
 import com.feldmann.projetofinalcdm.controller.ViewController;
 
@@ -11,6 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     private final String tagLog = this.getClass().getName().toString();
     private Controller.msg msg;
     private Controller.view view;
+    private Controller.controllerLogin login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +33,6 @@ public class LoginActivity extends AppCompatActivity {
     private void instanceController() {
         this.view = new ViewController(this, this);
         this.msg = new MsgController(view.getContext(), tagLog);
+        this.login = new LoginController(msg);
     }
 }
