9748070129cbf9f39bcbcd9e1ab5ec60240751fb
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-04 15:59:54 -0300

    feature/ criado construtor, e alterado metodos

--- app/src/main/java/com/feldmann/projetofinalcdm/controller/LoginController.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/controller/LoginController.java
@@ -1,20 +1,32 @@
 package com.feldmann.projetofinalcdm.controller;
 
-import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-
-import com.feldmann.projetofinalcdm.views.ListaActivity;
+import android.widget.EditText;
 
 public class LoginController implements Controller.controllerLogin{
+    private Controller.msg msg;
+
+    public LoginController(Controller.msg msg) {
+        this.msg = msg;
+    }
+
     @Override
-    public void paraTelaLista(Button btn) {
+    public void cadastrarUser(Button btn) {
+
+    }
+
+    @Override
+    public void verificaUser(Button btn) {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent in = new Intent(v.getContext(), ListaActivity.class);
-                v.getContext().startActivity(in);
+
             }
         });
+    }
+
+    @Override
+    public void paraTelaLista(EditText etLogin, EditText etSenha) {
     }
 }
