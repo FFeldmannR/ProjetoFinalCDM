332bc70d220932379f64802d484cf5a782a72fc9
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-04 18:24:38 -0300

    feature/ ajustado estrutura padrao

--- app/src/main/java/com/feldmann/projetofinalcdm/views/ListaActivity.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/ListaActivity.java
@@ -3,12 +3,32 @@ package com.feldmann.projetofinalcdm.views;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.feldmann.projetofinalcdm.R;
+import com.feldmann.projetofinalcdm.controller.Controller;
+import com.feldmann.projetofinalcdm.controller.MsgController;
+import com.feldmann.projetofinalcdm.controller.ViewController;
+import com.feldmann.projetofinalcdm.repository.DBListas;
 //
 public class ListaActivity extends AppCompatActivity {
+    private Controller.msg msg;
+    private Controller.view view;
+    private DBListas db;
     //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
+        this.instanceController();
+        db = new DBListas(view.getContext());
+        msg.logD("onCreate");
     }//fim onCreate
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        msg.logD("onResume");
+    }//fim onResume
+    private void instanceController() {
+        this.view = new ViewController(this, this);
+        this.msg = new MsgController(view.getContext(), this.getClass().getName().toString() );
+    }//fim instanceController
 }//fim class
