61b9f0b5479542faf069b788bd050d68edeac20c
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-04 16:07:24 -0300

    feature/ criado estrutura padrao

--- app/src/main/java/com/feldmann/projetofinalcdm/views/CadastroActivity.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/CadastroActivity.java
@@ -1,14 +1,33 @@
 package com.feldmann.projetofinalcdm.views;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.feldmann.projetofinalcdm.R;
+import com.feldmann.projetofinalcdm.controller.Controller;
+import com.feldmann.projetofinalcdm.controller.MsgController;
+import com.feldmann.projetofinalcdm.controller.ViewController;
 
 public class CadastroActivity extends AppCompatActivity {
+    private final String tagLog = this.getClass().getName().toString();
+    private Controller.msg msg;
+    private Controller.view view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        this.instanceController();
+        msg.logD("onCreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        msg.logD("onResume");
+    }
+
+    private void instanceController() {
+        this.view = new ViewController(this, this);
+        this.msg = new MsgController(view.getContext(), tagLog);
     }
 }
