7d312677698bcf2a6d07f523833cda4b536262c2
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-04 15:43:36 -0300

    feature/ criado activity lista

---
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/ListaActivity.java
@@ -0,0 +1,14 @@
+package com.feldmann.projetofinalcdm.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.feldmann.projetofinalcdm.R;
+
+public class ListaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+    }
+}
