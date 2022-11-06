0c3e5790274062e15b2c629c892abb9cdec07679
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-06 12:27:15 -0300

    feature/ adicionado atributo, construtor e get/set de donoLista

--- app/src/main/java/com/feldmann/projetofinalcdm/model/Listas.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/model/Listas.java
@@ -2,15 +2,20 @@ package com.feldmann.projetofinalcdm.model;
 
 public class Listas {
     private int id;
+    private String donoLista;
     private String nomeLista;
     //
-    public Listas(int id, String nomeLista) {
+    public Listas(int id, String donoLista, String nomeLista) {
         this.id = id;
+        this.donoLista = donoLista;
         this.nomeLista = nomeLista;
     }
     //
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
+
+    public String getDonoLista() { return donoLista; }
+    public void setDonoLista(String donoLista) { this.donoLista = donoLista; }
     //
     public String getNomeLista() { return nomeLista; }
     public void setNomeLista(String nomeLista) { this.nomeLista = nomeLista; }
