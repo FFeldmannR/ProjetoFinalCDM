7d8a5131feb180dc569ff1b67e8b39f12714a9f3
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-06 16:10:06 -0300

    feature/ criado activity para lista de compras

---
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/ListadeCompras.java
@@ -0,0 +1,33 @@
+package com.feldmann.projetofinalcdm.views;
+//
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.feldmann.projetofinalcdm.R;
+import com.feldmann.projetofinalcdm.controller.Controller;
+import com.feldmann.projetofinalcdm.controller.MsgController;
+import com.feldmann.projetofinalcdm.controller.ViewController;
+//
+public class ListadeCompras extends AppCompatActivity implements Controller.controllerInstance{
+    private Controller.msg msg;
+    private Controller.view view;
+    //
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listade_compras);
+        this.instanceController();
+        msg.logD("onCreate");
+    }
+    //
+    @Override
+    protected void onResume() {
+        super.onResume();
+        msg.logD("onResume");
+    }
+    //
+    @Override
+    public void instanceController() {
+        this.view = new ViewController(this, this);
+        this.msg = new MsgController(view.getContext(), this.getClass().getName().toString() );
+    }
+}
