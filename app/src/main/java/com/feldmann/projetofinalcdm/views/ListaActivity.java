9f92d734e8cd86ff134b53affa0aba319818489c
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-06 14:41:14 -0300

    feature/ feito a chamada de setAdapter

--- app/src/main/java/com/feldmann/projetofinalcdm/views/ListaActivity.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/ListaActivity.java
@@ -1,14 +1,12 @@
 package com.feldmann.projetofinalcdm.views;
 //
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.RecyclerView;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.ImageButton;
-
 import com.feldmann.projetofinalcdm.R;
 import com.feldmann.projetofinalcdm.controller.Controller;
-import com.feldmann.projetofinalcdm.controller.ListasController;
 import com.feldmann.projetofinalcdm.controller.MsgController;
 import com.feldmann.projetofinalcdm.controller.ViewController;
 import com.feldmann.projetofinalcdm.repository.DBListas;
@@ -17,8 +15,6 @@ import com.feldmann.projetofinalcdm.repository.ListasRepository;
 public class ListaActivity extends AppCompatActivity implements Controller.controllerInstance{
     private Controller.msg msg;
     private Controller.view view;
-    private ListasRepository listasRepository;
-    private ListasController listasController;
     private DBListas db;
     //
     @Override
@@ -34,24 +30,24 @@ public class ListaActivity extends AppCompatActivity implements Controller.contr
     protected void onResume() {
         super.onResume();
         msg.logD("onResume");
-        //
-        listasRepository.getInstance(view.getContext(), db.getWritableDatabase() );
-        listasController.addToDB(
+        ListasRepository.getInstanceListas(view.getContext(), db.getWritableDatabase() );
+        ListasRepository.addToDB(
                 ((ImageButton) findViewById(R.id.imgBtnAddList)),
                 getIntent().getStringExtra("NOMEUSER"),
-                getCountDB()//tamanho db
+                getNomeLista(),//tamanho db
+                db.getWritableDatabase()
         );
+        //
+        ListasRepository.setAdapterListas( (RecyclerView) findViewById(R.id.RVListas) );
+        //
     }//fim onResume
     @Override
     public void instanceController() {
         this.view = new ViewController(this, this);
         this.msg = new MsgController(view.getContext(), this.getClass().getName().toString() );
-        this.listasRepository = new ListasRepository(view.getContext() );
-        this.listasController = new ListasController();
     }//fim instanceController
-    private String getCountDB(){
-        //
+    private String getNomeLista(){
         Cursor cursor = db.getReadableDatabase().rawQuery("SELECT * FROM listas", null);
-        return Integer.valueOf( ( cursor.getCount() )+1 ).toString();
+        return "Lista "+( Integer.valueOf( ( cursor.getCount() )+1 ).toString() );
     }
 }//fim class
