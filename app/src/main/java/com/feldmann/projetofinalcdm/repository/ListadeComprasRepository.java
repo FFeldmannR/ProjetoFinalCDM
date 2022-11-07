3532f3224d96427c3a993ee39e513b315dbfe578
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-11-07 11:22:49 -0300

    feature/ inserido atributos

--- app/src/main/java/com/feldmann/projetofinalcdm/repository/ListadeComprasRepository.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/repository/ListadeComprasRepository.java
@@ -1,4 +1,13 @@
 package com.feldmann.projetofinalcdm.repository;
 
+import android.content.Context;
+import com.feldmann.projetofinalcdm.controller.Controller;
+import com.feldmann.projetofinalcdm.model.Listas;
+import java.util.List;
+
 public class ListadeComprasRepository {
+    private static ListasRepository instance = null;
+    private Context contexto;
+    private static List<Listas> listas;
+    private static Controller.msg msg;
 }
