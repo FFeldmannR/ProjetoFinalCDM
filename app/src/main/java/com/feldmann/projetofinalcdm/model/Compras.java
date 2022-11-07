4ee97844f3d00f79af08712a94d8537426318d81
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-11-07 11:34:23 -0300

    feature/ adicionado novo atributo, colocado no construtor e criado get/set

--- app/src/main/java/com/feldmann/projetofinalcdm/model/Compras.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/model/Compras.java
@@ -2,12 +2,14 @@ package com.feldmann.projetofinalcdm.model;
 
 public class Compras {
     private int id;
+    private String nomeLista;
     private String nomeItem;
     private String quantidade;
     private boolean completed;
     //
-    public Compras(int id, String nomeItem, String quantidade, boolean completed) {
+    public Compras(int id, String nomeLista, String nomeItem, String quantidade, boolean completed) {
         this.id = id;
+        this.nomeLista = nomeLista;
         this.nomeItem = nomeItem;
         this.quantidade = quantidade;
         this.completed = completed;
@@ -15,6 +17,9 @@ public class Compras {
     //
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
+    //
+    public String getNomeLista() { return nomeLista; }
+    public void setNomeLista(String nomeLista) { this.nomeLista = nomeLista; }
     //
     public String getNomeItem() { return nomeItem; }
     public void setNomeItem(String nomeItem) { this.nomeItem = nomeItem; }
