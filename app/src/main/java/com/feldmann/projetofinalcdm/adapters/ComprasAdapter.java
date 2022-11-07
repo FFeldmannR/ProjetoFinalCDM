234fa4925b3fd92db9b109b410d4bcec7bd658bf
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-11-07 12:08:32 -0300

    feature/ criado adapter para compras e implementado alguns metodos

--- app/src/main/java/com/feldmann/projetofinalcdm/adapters/ComprasAdapter.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/adapters/ComprasAdapter.java
@@ -1,11 +1,13 @@
 package com.feldmann.projetofinalcdm.adapters;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,32 +22,44 @@ import java.util.List;
 public class ComprasAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     //
     private List<Compras> compras;
+    private SQLiteDatabase sqlWrite;
     //
-    public ComprasAdapter(List<Compras> compras) {
+    public ComprasAdapter(List<Compras> compras, SQLiteDatabase sqlWrite) {
         this.compras = compras;
+        this.sqlWrite = sqlWrite;
     }
     //
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View layoutViewHolder = LayoutInflater.from(parent.getContext()).inflate(R.layout.modelo_item_layout, parent, false);
-        return new ListaViewHolder(layoutViewHolder);
+        return new ComprasViewHolder(layoutViewHolder);
     }
     //
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         Compras objCompras = compras.get(position);
         //
-        //nome item
-        ((TextView)((ListaViewHolder) holder).view.findViewById(R.id.tvnome)).setText( objCompras.getNomeItem() );
-        //quantidade
+        ((TextView)((ComprasViewHolder) holder).view.findViewById(R.id.tvNomeItem)).setText( objCompras.getNomeItem() );
+        ((TextView)((ComprasViewHolder) holder).view.findViewById(R.id.tvQuantidade)).setText( objCompras.getQuantidade() );
         //checkbox
+        this.itemClick(holder);
         //
     }
     //
     @Override
     public int getItemCount() {
         return compras.size();
+    }
+    //
+    private void itemClick(RecyclerView.ViewHolder holder){
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((CheckBox)((ComprasViewHolder) holder).view.findViewById(R.id.checkBoxItem)).setChecked(true);
+                // update no db
+            }
+        });
     }
     //
     private void buttonEffect(Context context, RecyclerView.ViewHolder holder){
