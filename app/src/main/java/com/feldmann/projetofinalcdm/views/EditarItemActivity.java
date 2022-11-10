45af19ee7e2a7ceecaae67feb62a2d2ed7c43d5b
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-11-10 11:46:59 -0300

    feature/ criado activity para editar item

---
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/EditarItemActivity.java
@@ -0,0 +1,36 @@
+package com.feldmann.projetofinalcdm.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.feldmann.projetofinalcdm.*;
+import com.feldmann.projetofinalcdm.controller.Controller;
+import com.feldmann.projetofinalcdm.controller.MsgController;
+import com.feldmann.projetofinalcdm.controller.ViewController;
+import com.feldmann.projetofinalcdm.repository.DBListas;
+
+public class EditarItemActivity extends AppCompatActivity implements Controller.controllerInstance {
+    private Controller.msg msg;
+    private Controller.view view;
+    private DBListas db;
+    //
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_item);
+        this.instanceController();
+        db = new DBListas(view.getContext());
+        msg.logD("onCreate");
+    }
+    @Override protected void onResume() {
+        super.onResume();
+        msg.logD("onResume");
+    }
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        msg.logD("onDestroy");
+    }
+    @Override public void instanceController() {
+        this.view = new ViewController(this, this);
+        this.msg = new MsgController(view.getContext(), this.getClass().getName() );
+    }
+}
