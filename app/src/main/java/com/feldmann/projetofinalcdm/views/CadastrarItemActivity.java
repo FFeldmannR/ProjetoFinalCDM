0122d0b1d065b43e4fd8bf5d2da52a036f480b85
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-11-10 13:12:44 -0300

    refactor/ adicionado parametro em addItemDatabase() do usuario logado

--- app/src/main/java/com/feldmann/projetofinalcdm/views/CadastrarItemActivity.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/CadastrarItemActivity.java
@@ -2,12 +2,9 @@ package com.feldmann.projetofinalcdm.views;
 //
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import com.feldmann.projetofinalcdm.R;
@@ -40,7 +37,7 @@ public class CadastrarItemActivity extends AppCompatActivity implements Controll
         ComprasRepository.addItemToDB(
                 db.getWritableDatabase(),
                 (Button) findViewById(R.id.btnSalvar),
-                nomeLista,
+                getIntent().getStringExtra("USUARIOLOGADO"), nomeLista,
                 (EditText) findViewById(R.id.etNomeItem),
                 (EditText) findViewById(R.id.etQuantidade),
                 0
