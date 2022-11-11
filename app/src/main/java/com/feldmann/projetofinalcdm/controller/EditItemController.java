a6315d8497f7502eb025bbcfd46891d8b7aaaf99
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-11-11 09:44:00 -0300

    feature/ criado metodos updateItem e deleteItem

--- app/src/main/java/com/feldmann/projetofinalcdm/controller/EditItemController.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/controller/EditItemController.java
@@ -1,4 +1,35 @@
 package com.feldmann.projetofinalcdm.controller;
 
-public class EditItemController {
-}
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class EditItemController implements Controller.controllerEditItem{
+    //
+    @Override public void updateItem(SQLiteDatabase sqlWrite, String novoNomeItem,
+                           String novaQuantidade, String donoLista,
+                           String nomeLista) {
+        try{
+            sqlWrite.execSQL(
+                    "UPDATE compras"+
+                    " SET nomeItem='"+novoNomeItem+"'"+
+                    " AND quantidade='"+novaQuantidade+"'"+
+                    " WHERE donoLista='"+donoLista+"'"+
+                    " AND nomeLista='"+nomeLista+"'" );
+        }catch (Exception e){
+            Log.d("EDIT_ITEM", "FALHA AO ATUALIZAR\n"+e.getMessage() );
+        }
+    }//fim metodo updateItem
+    //
+    @Override public void deleteItem(SQLiteDatabase sqlWrite, String donoLista,
+                           String nomeLista, String nomeItem) {
+        try{
+            sqlWrite.execSQL(
+                    "DELETE FROM compras"+
+                    " WHERE donoLista='"+donoLista+"'"+
+                    " AND nomeLista='"+nomeLista+"'"+
+                    " AND nomeItem='"+nomeItem+"'" );
+        }catch (Exception e){
+            Log.d("EDIT_ITEM", "FALHA AO DELETAR\n"+e.getMessage() );
+        }
+    }//fim metodo deleteItem
+}//fim class
