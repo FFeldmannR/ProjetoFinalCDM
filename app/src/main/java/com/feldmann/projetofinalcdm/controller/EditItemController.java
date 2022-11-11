48c107d6cb8fa5efe6a8b28a8978f0e804a3e7bd
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-11-11 09:54:55 -0300

    feature/ colocado listener dos botoes nos metodos e um setColor pro botao deletar

--- app/src/main/java/com/feldmann/projetofinalcdm/controller/EditItemController.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/controller/EditItemController.java
@@ -1,35 +1,41 @@
 package com.feldmann.projetofinalcdm.controller;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class EditItemController implements Controller.controllerEditItem{
     //
-    @Override public void updateItem(SQLiteDatabase sqlWrite, String novoNomeItem,
-                           String novaQuantidade, String donoLista,
-                           String nomeLista) {
-        try{
-            sqlWrite.execSQL(
-                    "UPDATE compras"+
-                    " SET nomeItem='"+novoNomeItem+"'"+
-                    " AND quantidade='"+novaQuantidade+"'"+
-                    " WHERE donoLista='"+donoLista+"'"+
-                    " AND nomeLista='"+nomeLista+"'" );
-        }catch (Exception e){
-            Log.d("EDIT_ITEM", "FALHA AO ATUALIZAR\n"+e.getMessage() );
-        }
+    @Override public void updateItem(Button btnSalvar, SQLiteDatabase sqlWrite,
+                                     String novoNomeItem, String novaQuantidade,
+                                     String donoLista, String nomeLista) {
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                try{ sqlWrite.execSQL(
+                            "UPDATE compras"+
+                            " SET nomeItem='"+novoNomeItem+"'"+
+                            " AND quantidade='"+novaQuantidade+"'"+
+                            " WHERE donoLista='"+donoLista+"'"+
+                            " AND nomeLista='"+nomeLista+"'" );
+                }catch (Exception e){ Log.d("EDIT_ITEM", "FALHA AO ATUALIZAR\n"+e.getMessage() ); }//fim try catch
+            }//fim onClick
+        });//fim clickListener
     }//fim metodo updateItem
     //
-    @Override public void deleteItem(SQLiteDatabase sqlWrite, String donoLista,
-                           String nomeLista, String nomeItem) {
-        try{
-            sqlWrite.execSQL(
-                    "DELETE FROM compras"+
-                    " WHERE donoLista='"+donoLista+"'"+
-                    " AND nomeLista='"+nomeLista+"'"+
-                    " AND nomeItem='"+nomeItem+"'" );
-        }catch (Exception e){
-            Log.d("EDIT_ITEM", "FALHA AO DELETAR\n"+e.getMessage() );
-        }
+    @Override public void deleteItem(Button btnDeletar, SQLiteDatabase sqlWrite,
+                                     String donoLista, String nomeLista, String nomeItem) {
+        btnDeletar.setBackgroundColor(Color.RED); //seta a cor do botao delete para vermelho
+        btnDeletar.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                try{ sqlWrite.execSQL(
+                            "DELETE FROM compras"+
+                            " WHERE donoLista='"+donoLista+"'"+
+                            " AND nomeLista='"+nomeLista+"'"+
+                            " AND nomeItem='"+nomeItem+"'" );
+                }catch (Exception e){ Log.d("EDIT_ITEM", "FALHA AO DELETAR\n"+e.getMessage() ); }
+            }//fim onClick
+        });//fim clickListener
     }//fim metodo deleteItem
 }//fim class
