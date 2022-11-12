43a91910d8955f03c565330d8f7b75a39ec0ea08
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-12 14:42:04 -0300

    perf/ criado metodos de inserção de dados

--- app/src/main/java/com/feldmann/projetofinalcdm/controller/CadastroController.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/controller/CadastroController.java
@@ -1,49 +1,35 @@
 package com.feldmann.projetofinalcdm.controller;
 //
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import com.feldmann.projetofinalcdm.views.LoginActivity;
+import com.feldmann.projetofinalcdm.repository.*;
+import com.feldmann.projetofinalcdm.views.*;
 //
 public class CadastroController implements Controller.controllerCadastro{
     private Controller.msg msg;
-    private EditText nome, senha;
+    private Context context;
     //
     public CadastroController(Context context) {
-        this.msg = new MsgController(context, this.getClass().getName().toString() );
+        this.msg = new MsgController(context, this.getClass().getName() );
+        this.context = context;
     }
-    //
-    @Override
-    public void getCampos(EditText etNome, EditText etSenha) {
-        this.nome = etNome;
-        this.senha = etSenha;
+    @Override public void addItemToDB(String usuarioLogado, String nomeLista, String nomeItem, String qntdItem, int completed) {
+        ComprasRepository.createItem( usuarioLogado, nomeLista, nomeItem, qntdItem, completed );
+        Intent in = new Intent( context, ListadeCompras.class );
+        in.putExtra("USUARIO", usuarioLogado );
+        in.putExtra("NOMELISTA", nomeLista );
+        context.startActivity(in);
     }
-    //
-    @Override
-    public void addUserToDB(Button btnCadastrar, SQLiteDatabase sqlWrite) {
-        btnCadastrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ContentValues ctv = new ContentValues();
-                try{
-                    //
-                    String nomeUser = nome.getText().toString();
-                    ctv.put("nome", nomeUser );
-                    ctv.put("senha", senha.getText().toString() );
-                    sqlWrite.insert("users", null, ctv);
-                    msg.logD(nomeUser + " ADICIONADO");
-                    Intent in = new Intent(v.getContext(), LoginActivity.class);
-                    in.putExtra("NOMEUSER", nomeUser);
-                    v.getContext().startActivity(in);
-                }catch (Exception e){
-                    msg.messageToast("ERRO AO ADICIONAR USUARIO");
-                    msg.logD("ERRO!!! "+e.getMessage() );
-                }//fim try catch
-            }//fim onClick
-        });//fim setClickListener
+    @Override public void addUserToDB( String nomeUser, String senhaUser ) {
+        UserRepository.createUserinDB( nomeUser, senhaUser );
+        Intent in = new Intent( context, LoginActivity.class );
+        in.putExtra("NOMEUSER", nomeUser );
+        context.startActivity(in);
     }//fim addToDB
+    @Override public void addListToDB(String donoLista, String nomeLista) {
+        ListasRepository.insertNewListToDB( donoLista, nomeLista );
+        Intent in = new Intent( context, ListaActivity.class );
+        in.putExtra("NOMEUSER", donoLista );
+        context.startActivity(in);
+    }
 }//fim class
