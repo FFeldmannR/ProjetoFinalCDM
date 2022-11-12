f5a721e40e93434632ec48fdeb6e15a8f3e862c1
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-12 19:13:19 -0300

    feature/ criado getInstance

--- app/src/main/java/com/feldmann/projetofinalcdm/repository/UserRepository.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/repository/UserRepository.java
@@ -7,22 +7,27 @@ import com.feldmann.projetofinalcdm.controller.Controller;
 import com.feldmann.projetofinalcdm.controller.MsgController;
 //
 public class UserRepository {
+    private static UserRepository instance = null;
     private static Controller.msg msg;
     private static DBListas db;
     //
     public UserRepository(Context context) {
-        msg = new MsgController( context, this.getClass().getName().toString() );
+        msg = new MsgController( context, this.getClass().getName() );
         db = new DBListas( context );
     }//fim construtor
+    public static UserRepository getInstance(Context context) {
+        instance = new UserRepository( context );
+        return instance;
+    }//fim getInstance
     public static void createUserinDB(String nomeUser, String senhaUser){
         try{
             ContentValues ctv = new ContentValues();
             ctv.put("nome", nomeUser );
             ctv.put("senha", senhaUser );
             db.getWritableDatabase().insert("users", null, ctv);
-            msg.logD(nomeUser + " ADICIONADO");
+            msg.messageToast(nomeUser + " ADICIONADO");
         }catch (SQLException sqlE){
-            msg.messageToast("ERRO AO ADICIONAR USUARIO\n"+sqlE.getMessage());
+            msg.logD("ERRO AO ADICIONAR USUARIO\n"+sqlE.getMessage());
         }//fim try catch
     }//fim createUserinDB
 }//fim classe
