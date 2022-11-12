0dc30d7b5760ec67d0595e8d28bb60f7b9b0ad75
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-12 19:09:57 -0300

    feature/ adicionado metodos para acitivity de edit da lista

--- app/src/main/java/com/feldmann/projetofinalcdm/views/EditListActivity.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/EditListActivity.java
@@ -3,26 +3,45 @@ package com.feldmann.projetofinalcdm.views;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import com.feldmann.projetofinalcdm.R;
 import com.feldmann.projetofinalcdm.controller.*;
 //
 public class EditListActivity extends AppCompatActivity implements Controller.controllerInstance {
     private Controller.msg msg;
     private Controller.view view;
+    private Controller.controllerEditList editList;
     private String usuarioLogado, nomeLista;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_list);
+        this.instanceController();
         msg.logD("onCreate");
     }//fim onCreate
     @Override protected void onResume() {
         super.onResume();
+        this.toolBar();
         msg.logD("onResume");
         usuarioLogado = getIntent().getStringExtra("USUARIOLOGADO");
         nomeLista = getIntent().getStringExtra("NOMELISTA");
+        //
+        EditText etNomeLista = ((EditText) findViewById(R.id.etNomeListaEdit));
+        etNomeLista.setText(nomeLista);
+        //
+        Button btnSalvar = ((Button) findViewById(R.id.btnSalvarList));
+        btnSalvar.setBackgroundColor(Color.GREEN);
+        btnSalvar.setTextColor(Color.BLACK);
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                editList.updateList( usuarioLogado, nomeLista, etNomeLista.getText().toString() );
+            }//fim onClick
+        });//fim clickListener
     }//fim onResume
     @Override protected void onDestroy() {
         super.onDestroy();
@@ -30,7 +49,8 @@ public class EditListActivity extends AppCompatActivity implements Controller.co
     }//fim onDestroy
     @Override public void instanceController() {
         this.view = new ViewController(this, this);
-        this.msg = new MsgController(view.getContext(), this.getClass().getName() );
+        this.msg = new MsgController( view.getContext(), this.getClass().getName() );
+        this.editList = new EditListController( view.getContext() );
     }//fim instanceController
             // METODOS PARA OS BOTOES VOLTAR
     private void toolBar(){
