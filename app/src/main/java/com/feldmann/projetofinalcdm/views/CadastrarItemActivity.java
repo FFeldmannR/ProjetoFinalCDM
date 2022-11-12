654e41187f37525abebdcd041760e83c524b0a76
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-12 14:41:39 -0300

    perf/ ajustado identação e organização do codigo

--- app/src/main/java/com/feldmann/projetofinalcdm/views/CadastrarItemActivity.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/CadastrarItemActivity.java
@@ -5,57 +5,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import com.feldmann.projetofinalcdm.R;
-import com.feldmann.projetofinalcdm.controller.Controller;
-import com.feldmann.projetofinalcdm.controller.MsgController;
-import com.feldmann.projetofinalcdm.controller.ViewController;
-import com.feldmann.projetofinalcdm.repository.ComprasRepository;
-import com.feldmann.projetofinalcdm.repository.DBListas;
+import com.feldmann.projetofinalcdm.controller.*;
 //
 public class CadastrarItemActivity extends AppCompatActivity implements Controller.controllerInstance {
     private Controller.msg msg;
     private Controller.view view;
-    private DBListas db;
-    String nomeLista;
+    private Controller.controllerCadastro cadastro;
+    private String nomeLista, usuarioLogado;
     //
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_item);
         this.instanceController();
-        db = new DBListas(view.getContext());
         msg.logD("onCreate");
     }
-    //
     @Override protected void onResume() {
         super.onResume();
         msg.logD("onResume");
         this.toolBar();
+        usuarioLogado = getIntent().getStringExtra("USUARIOLOGADO");
         nomeLista = getIntent().getStringExtra("NOMELISTA");
         setTitle("Adicionar item em "+nomeLista);
-        ComprasRepository.addItemToDB(
-                db.getWritableDatabase(),
-                (Button) findViewById(R.id.btnSalvar),
-                getIntent().getStringExtra("USUARIOLOGADO"), nomeLista,
-                (EditText) findViewById(R.id.etNomeItem),
-                (EditText) findViewById(R.id.etQuantidade),
-                0
-        );
+        ((Button) findViewById(R.id.btnSalvar)).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                cadastro.addItemToDB(
+                        usuarioLogado, nomeLista,
+                        ((EditText) findViewById(R.id.etNomeItem)).getText().toString(),
+                        ((EditText) findViewById(R.id.etQuantidade)).getText().toString(),
+                        0 );
+            }//fim onClick
+        });//fim clickListener
     }//fim onResume
     @Override protected void onDestroy() {
         super.onDestroy();
         msg.logD("onDestroy");
     }
-    //
     @Override public void instanceController() {
         this.view = new ViewController(this, this);
         this.msg = new MsgController(view.getContext(), this.getClass().getName() );
+        this.cadastro = new CadastroController( view.getContext() );
     }
+            // METODOS PARA OS BOTOES VOLTAR
     private void toolBar(){
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-    }
+    }//fim toolBar()
     @Override public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case android.R.id.home:
@@ -66,13 +64,10 @@ public class CadastrarItemActivity extends AppCompatActivity implements Controll
                     msg.logD("nova activity iniciada: ListadeCompras");
                     finishAffinity();
                     break;
-                }catch (Exception e){
-                    msg.logD("FALHA AO VOLTAR\nERRO::: "+e.getMessage());
-                }
-
-        }
+                }catch (Exception e){ msg.logD("FALHA AO VOLTAR\nERRO::: "+e.getMessage()); }//fim try catch
+        }//fim switch
         return true;
-    }
+    }//fim onOptionsItemSelected
     @Override public void onBackPressed() {
         try {
             Intent intent = new Intent(this, ListadeCompras.class);
@@ -80,8 +75,6 @@ public class CadastrarItemActivity extends AppCompatActivity implements Controll
             startActivity(intent);
             msg.logD("nova activity iniciada: ListadeCompras");
             finishAffinity();
-        }catch (Exception e){
-            msg.logD("FALHA AO VOLTAR\nERRO::: "+e.getMessage());
-        }
-    }
+        }catch (Exception e){ msg.logD("FALHA AO VOLTAR\nERRO::: "+e.getMessage()); }
+    }//fim onBackPressed
 }//fim CadastrarItemActivity
--- app/src/main/java/com/feldmann/projetofinalcdm/views/CadastroActivity.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/CadastroActivity.java
@@ -2,53 +2,49 @@ package com.feldmann.projetofinalcdm.views;
 //
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import com.feldmann.projetofinalcdm.R;
-import com.feldmann.projetofinalcdm.controller.CadastroController;
-import com.feldmann.projetofinalcdm.controller.Controller;
-import com.feldmann.projetofinalcdm.controller.MsgController;
-import com.feldmann.projetofinalcdm.controller.ViewController;
-import com.feldmann.projetofinalcdm.repository.DBListas;
+import com.feldmann.projetofinalcdm.controller.*;
 //
 public class CadastroActivity extends AppCompatActivity implements Controller.controllerInstance{
     private Controller.msg msg;
     private Controller.view view;
-    private DBListas db;
     private Controller.controllerCadastro cadastro;
     //
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
         this.instanceController();
-        db = new DBListas(view.getContext());
         msg.logD("onCreate");
     }//fim onCreate
-    //
     @Override protected void onResume() {
         super.onResume();
         msg.logD("onResume");
         this.toolBar();
-        cadastro.getCampos((EditText) findViewById(R.id.etLoginC), (EditText) findViewById(R.id.etSenhaC));
-        cadastro.addUserToDB((Button) findViewById(R.id.btnCadastrarC), db.getWritableDatabase());
+        ((Button) findViewById(R.id.btnCadastrarC)).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                cadastro.addUserToDB(
+                        ((EditText) findViewById(R.id.etLoginC)).getText().toString(),
+                        ((EditText) findViewById(R.id.etSenhaC)).getText().toString()
+                );//fim addUserToDB
+            }//fim onClick
+        });//fim clickListener
     }//fim onResume
-    //
     @Override protected void onDestroy() {
         super.onDestroy();
         msg.logD("onDestroy");
     }
-    //
     @Override public void instanceController() {
         this.view = new ViewController(this, this);
         this.msg = new MsgController(view.getContext(), this.getClass().getName().toString() );
         this.cadastro = new CadastroController(view.getContext());
     }//fim instanceController
-    //
+            // METODOS PARA OS BOTOES VOLTAR
     private void toolBar(){
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
