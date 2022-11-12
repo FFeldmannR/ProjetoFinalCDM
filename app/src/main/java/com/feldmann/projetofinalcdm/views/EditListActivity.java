8e23c346379476dc02a743438389cd03acfd98c9
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-12 15:56:48 -0300

    feature/ criado metodos padrao, e recebendo valores da activity anterior

--- app/src/main/java/com/feldmann/projetofinalcdm/views/EditListActivity.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/EditListActivity.java
@@ -1,16 +1,61 @@
 package com.feldmann.projetofinalcdm.views;
-
+//
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.MenuItem;
 import com.feldmann.projetofinalcdm.R;
+import com.feldmann.projetofinalcdm.controller.*;
+//
+public class EditListActivity extends AppCompatActivity implements Controller.controllerInstance {
+    private Controller.msg msg;
+    private Controller.view view;
+    private String usuarioLogado, nomeLista;
 
-public class EditListActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_list);
-    }
-}
+        msg.logD("onCreate");
+    }//fim onCreate
+    @Override protected void onResume() {
+        super.onResume();
+        msg.logD("onResume");
+        usuarioLogado = getIntent().getStringExtra("USUARIOLOGADO");
+        nomeLista = getIntent().getStringExtra("NOMELISTA");
+    }//fim onResume
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        msg.logD("onDestroy");
+    }//fim onDestroy
+    @Override public void instanceController() {
+        this.view = new ViewController(this, this);
+        this.msg = new MsgController(view.getContext(), this.getClass().getName() );
+    }//fim instanceController
+            // METODOS PARA OS BOTOES VOLTAR
+    private void toolBar(){
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }//fim toolBar()
+    @Override public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch ( item.getItemId() ) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, ListaActivity.class);
+                intent.putExtra("NOMEUSER", usuarioLogado);
+                startActivity( intent );
+                finish();
+                break;
+        }//fim switch
+        return true;
+    }//fimonOptionsItemSelected
+    @Override public void onBackPressed() {
+        try {
+            Intent intent = new Intent(this, ListaActivity.class);
+            intent.putExtra("NOMEUSER", usuarioLogado);
+            startActivity( intent );
+            finishAffinity();
+        }catch (Exception e){
+            msg.logD("FALHA AO VOLTAR\nERRO::: "+e.getMessage());
+        }//fim try catch
+    }//fim onBackPressed
+}//fim class
