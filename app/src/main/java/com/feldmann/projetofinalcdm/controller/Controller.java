882fef7417753b421739ec146cef146e9de0d30d
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-12 14:45:28 -0300

    perf/ criado novos metodos para melhor performance

--- app/src/main/java/com/feldmann/projetofinalcdm/controller/Controller.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/controller/Controller.java
@@ -3,16 +3,10 @@ package com.feldmann.projetofinalcdm.controller;
 import android.app.Activity;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
+import android.widget.*;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.feldmann.projetofinalcdm.model.Compras;
-
+import java.util.List;
 //
 public class Controller {
     public interface controllerInstance{
@@ -30,17 +24,19 @@ public class Controller {
         public String criarTabelaUsers();
         public String criarTabelaListas();
         public String criarTabelaCompras();
-        public void selectTable(SQLiteDatabase sqlRead, String nomeTabela);
     }
     public interface controllerLogin{
         public void setLoginField(String nomeUser, EditText etLoginL);
         public void setTvEMS(TextView tvEMS);
-        public void Login(Button btn, EditText etLogin, EditText etSenha);
+        public void Login(String login, String senha);
         public void cadastrarUser(Button btn);
     }
     public interface controllerCadastro{
-        public void getCampos(EditText etNome, EditText etSenha);
-        public void addUserToDB(Button btnCadastrar, SQLiteDatabase sqlWrite);
+        public void addItemToDB(String usuarioLogado, String nomeLista,
+                                String nomeItem, String qntdItem,
+                                int completed);
+        public void addUserToDB( String nomeUser, String senhaUser );
+        public void addListToDB(String donoLista, String nomeLista);
     }
     public interface controllerComprasAdapter{
         public void selectList(Compras objCompras);
@@ -50,10 +46,11 @@ public class Controller {
         public void clickInfoItemToEdit(LinearLayout infoItem, RecyclerView.ViewHolder holder, Compras compras);
     }
     public interface controllerEditItem{
-        public void updateItem(Button btnSalvar, SQLiteDatabase sqlWrite,
-                               EditText novoNomeItem, EditText novaQuantidade,
-                               Compras objCompras);
-        public void deleteItem(Button btnDeletar, SQLiteDatabase sqlWrite,
-                               String donoLista, String nomeLista, String nomeItem);
+        public void updateItem(String novoNomeItem, String novaQuantidade, Compras objCompras);
+        public void deleteItem(Compras objCompras);
     }
-}
+    public interface controllerAdapters{
+        public void setAdapterItemList(RecyclerView rv, List<Compras> comprasList,
+                                       SQLiteDatabase sqlWrite, String lista);
+    }
+}//fim classe
