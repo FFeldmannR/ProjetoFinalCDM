86f4b0c1cc14c8847dab93fd013ff8d2d51b1743
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-15 11:43:43 -0300

    refactor/ alterado metodo de criar usuario ao tentar criar usuario ja existente, exibe mensagem de usuario ja existe

--- app/src/main/java/com/feldmann/projetofinalcdm/repository/UserRepository.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/repository/UserRepository.java
@@ -2,17 +2,26 @@ package com.feldmann.projetofinalcdm.repository;
 //
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
+import android.graphics.Color;
+import android.widget.TextView;
+
+import com.feldmann.projetofinalcdm.R;
 import com.feldmann.projetofinalcdm.controller.Controller;
 import com.feldmann.projetofinalcdm.controller.MsgController;
+import com.feldmann.projetofinalcdm.views.LoginActivity;
+
 //
 public class UserRepository {
+    private static Context context;
     private static UserRepository instance = null;
     private static Controller.msg msg;
     private static DBListas db;
     //
     public UserRepository(Context context) {
+        this.context = context;
         msg = new MsgController( context, this.getClass().getName() );
         db = new DBListas( context );
     }//fim construtor
@@ -20,24 +29,33 @@ public class UserRepository {
         instance = new UserRepository( context );
         return instance;
     }//fim getInstance
-    public static void createUserinDB(String nomeUser, String senhaUser){
+    public static void createUserinDB(String nomeUser, String senhaUser, TextView tvIncorreto){
         Cursor c = db.getReadableDatabase().rawQuery("SELECT * FROM users", null);
         if (c.moveToFirst()){
             //
             do {
                 if ( nomeUser.equals( c.getString(1) ) ){
-                    msg.messageToast("USUARIO JA EXISTE");
+                    tvIncorreto.setTextColor(Color.RED);
+                    tvIncorreto.setText(context.getResources().getString(R.string.usuarioJaExiste));
                 }else{
-                    try{
-                        ContentValues ctv = new ContentValues();
-                        ctv.put("nome", nomeUser );
-                        ctv.put("senha", senhaUser );
-                        db.getWritableDatabase().insert("users", null, ctv);
-                        msg.messageToast(nomeUser + " ADICIONADO");
-                    }catch (SQLException sqlE){
-                        msg.logD("ERRO AO ADICIONAR USUARIO\n"+sqlE.getMessage());
-                    }//fim try catch
-                    break;
+                    if ( senhaUser.equals( c.getString(2) ) ){
+                        try{
+                            ContentValues ctv = new ContentValues();
+                            ctv.put("nome", nomeUser );
+                            ctv.put("senha", senhaUser );
+                            db.getWritableDatabase().insert("users", null, ctv);
+                            msg.messageToast(nomeUser + " ADICIONADO");
+                            Intent in = new Intent( context, LoginActivity.class );
+                            in.putExtra("NOMEUSER", nomeUser );
+                            context.startActivity(in);
+                        }catch (SQLException sqlE){
+                            msg.logD("ERRO AO ADICIONAR USUARIO\n"+sqlE.getMessage());
+                        }//fim try catch
+                        break;
+                    }/*else {
+                        tvIncorreto.setTextColor(Color.RED);
+                        tvIncorreto.setText(context.getResources().getString(R.string.senhaIncorreta));
+                    }*/
                 }
             }while (c.moveToNext());
         }else{
