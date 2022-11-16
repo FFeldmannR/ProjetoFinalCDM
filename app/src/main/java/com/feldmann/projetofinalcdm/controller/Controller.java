788e594bda7e631f4c542662e4b70b4ae9b246e6
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-16 18:43:39 -0300

    style/ ajustado codigo

--- app/src/main/java/com/feldmann/projetofinalcdm/controller/Controller.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/controller/Controller.java
@@ -2,59 +2,55 @@ package com.feldmann.projetofinalcdm.controller;
 //
 import android.app.Activity;
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
 import android.widget.*;
 import androidx.recyclerview.widget.RecyclerView;
-import com.feldmann.projetofinalcdm.model.Compras;
-
-import org.w3c.dom.Text;
-
+import com.feldmann.projetofinalcdm.model.*;
 import java.util.List;
 //
 public class Controller {
-    public interface controllerInstance{
-        public void instanceController();
+    public interface controllerInstance {
+        public void instanceController ();
     }
-    public interface view{
-        public Activity getActivity();
-        public Context getContext();
-        public void selectTableDB( String nomeTabela );
+    public interface view {
+        public Activity getActivity ();
+        public Context getContext ();
+        public void selectTableDB ( String nomeTabela );
     }
-    public interface msg{
-        public void messageToast(String msg);
-        public void logD(String msg);
+    public interface msg {
+        public void messageToast ( String msg );
+        public void logD ( String msg );
     }
-    public interface controllerDataBase{
-        public String criarTabelaUsers();
-        public String criarTabelaListas();
-        public String criarTabelaCompras();
+    public interface controllerDataBase {
+        public String criarTabelaUsers ();
+        public String criarTabelaListas ();
+        public String criarTabelaCompras ();
     }
-    public interface controllerLogin{
-        public void Login( String login, String senha, TextView tvIncorreto );
+    public interface controllerLogin {
+        public void Login ( String login, String senha, TextView tvIncorreto );
     }
-    public interface controllerCadastro{
-        public void addItemToDB( String usuarioLogado, String nomeLista,
+    public interface controllerCadastro {
+        public void addItemToDB ( String usuarioLogado, String nomeLista,
                                 String nomeItem, String qntdItem,
                                 int completed );
-        public void addUserToDB(String nomeUser, String senhaUser, TextView tvCadastroIncorreto );
-        public void addListToDB( String donoLista, String nomeLista );
+        public void addUserToDB ( String nomeUser, String senhaUser, TextView tvCadastroIncorreto );
+        public void addListToDB ( String donoLista, String nomeLista );
     }
-    public interface controllerComprasAdapter{
-        public void selectList(Compras objCompras);
-        public void updateCheckbox(RecyclerView.ViewHolder holder,
-                                   SQLiteDatabase sqlWrite, CheckBox checkBox,
-                                   String nomeListaAtual, Compras compras);
-        public void clickInfoItemToEdit(LinearLayout infoItem, RecyclerView.ViewHolder holder, Compras compras);
+    public interface controllerComprasAdapter {
+        public void selectList ( Compras objCompras );
+        public void updateCheckbox ( RecyclerView.ViewHolder holder, CheckBox checkBox,
+                                    String nomeListaAtual, Compras compras );
+        public void clickInfoItemToEdit ( LinearLayout infoItem, RecyclerView.ViewHolder holder, Compras compras );
     }
-    public interface controllerEditItem{
-        public void updateItem(String novoNomeItem, String novaQuantidade, Compras objCompras);
-        public void deleteItem(Compras objCompras);
+    public interface controllerEditItem {
+        public void updateItem ( String novoNomeItem, String novaQuantidade, Compras objCompras );
+        public void deleteItem ( Compras objCompras );
     }
-    public interface controllerEditList{
-        public void updateList( String donoLista, String nomeLista, String novoNomeLista, TextView tvNomeListaErrado );
-        public void deleteList( String donoLista, String nomeLista );
+    public interface controllerEditList {
+        public void updateList ( String donoLista, String nomeLista, String novoNomeLista, TextView tvNomeListaErrado );
+        public void deleteList ( String donoLista, String nomeLista );
     }
-    public interface controllerAdapters{
-        public void setAdapterItemList(RecyclerView rv, List<Compras> comprasList, String lista);
+    public interface controllerAdapters {
+        public void setAdapterItemList ( RecyclerView rv, List<Compras> compras, String lista );
+        public void setAdapterLists ( RecyclerView rv, List<Listas> listas, String usuarioLogado );
     }
 }//fim classe
