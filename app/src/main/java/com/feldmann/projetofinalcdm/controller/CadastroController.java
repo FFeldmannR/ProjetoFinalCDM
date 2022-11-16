8f5609f5614012ca23f1e868ef7fedf46a6d695c
Fabio Feldmann Rodrigues <91392541+iKickBR@users.noreply.github.com>
2022-11-16 18:49:08 -0300

    style/ ajustado codigo

--- app/src/main/java/com/feldmann/projetofinalcdm/controller/CadastroController.java
+++ app/src/main/java/com/feldmann/projetofinalcdm/controller/CadastroController.java
@@ -7,24 +7,24 @@ import android.widget.TextView;
 import com.feldmann.projetofinalcdm.repository.*;
 import com.feldmann.projetofinalcdm.views.*;
 //
-public class CadastroController implements Controller.controllerCadastro{
+public class CadastroController implements Controller.controllerCadastro {
     private Controller.msg msg;
     private Context context;
     //
-    public CadastroController(Context context) {
-        this.msg = new MsgController(context, this.getClass().getName() );
+    public CadastroController ( Context context ) {
+        this.msg = new MsgController ( context, this.getClass().getName() );
         this.context = context;
     }
-    @Override public void addItemToDB(String usuarioLogado, String nomeLista, String nomeItem, String qntdItem, int completed) {
-        ComprasRepository.createItem( usuarioLogado, nomeLista, nomeItem, qntdItem, completed );
+    @Override public void addItemToDB ( String usuarioLogado, String nomeLista, String nomeItem, String qntdItem, int completed ) {
+        ComprasRepository.createItem ( usuarioLogado, nomeLista, nomeItem, qntdItem, completed );
     }
-    @Override public void addUserToDB( String nomeUser, String senhaUser, TextView tvCadastroIncorreto ) {
-        UserRepository.createUserinDB( nomeUser, senhaUser, tvCadastroIncorreto );
+    @Override public void addUserToDB ( String nomeUser, String senhaUser, TextView tvCadastroIncorreto ) {
+        UserRepository.createUserinDB ( nomeUser, senhaUser, tvCadastroIncorreto );
     }//fim addToDB
-    @Override public void addListToDB( String donoLista, String nomeLista ) {
-        ListasRepository.createList( donoLista, nomeLista );
-        Intent in = new Intent( context, ListaActivity.class );
-        in.putExtra("NOMEUSER", donoLista );
-        context.startActivity(in);
+    @Override public void addListToDB ( String donoLista, String nomeLista ) {
+        ListasRepository.createList ( donoLista, nomeLista );
+        Intent in = new Intent ( context, ListaActivity.class );
+        in.putExtra ("NOMEUSER", donoLista );
+        context.startActivity ( in );
     }
 }//fim class
