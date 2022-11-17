22ed792ca945c09c3396b01c33a86a07d97608d1
fabio feldmann rodrigues <fabio.feldmann97@gmail.com>
2022-11-17 11:55:30 -0300

    feature/ criado activity para editar usuario

---
+++ app/src/main/java/com/feldmann/projetofinalcdm/views/EditUserActivity.java
@@ -0,0 +1,14 @@
+package com.feldmann.projetofinalcdm.views;
+//
+import iandroidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.feldmann.projetofinalcdm.R;
+//
+public class EditUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user);
+    }
+}
